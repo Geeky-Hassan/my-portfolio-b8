755764695acdb88234fb704ff4829cc7b5cf1841
Geeky Hassan <Geekyhassan@gmail.com>
2024-11-23 16:51:41 +0500

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Geeky-Hassan/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/geeky-hassan/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
